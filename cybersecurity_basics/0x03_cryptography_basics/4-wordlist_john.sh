030acf7c1cd908708b45fee88c32007477f1d415
IsaevThunder <12584@holbertonstudents.com>
2026-02-08 13:23:12 -0500

    holberton

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,1 @@
-#!/bin/bash
-john --format=sha256 --wordlist /usr/share/wordlists/rockyou.txt.gz "$1" ; john --show "$1" | cut -d: -f2 | head -n 1  > 4-password.txt
+john --format=raw-sha256 --wordlist=/usr/share/wordlists/rockyou_clean.txt "$1"; john --show --format=raw-sha256 "$1" | cut -d: -f2 | head -n 1 > 4-password.txt
