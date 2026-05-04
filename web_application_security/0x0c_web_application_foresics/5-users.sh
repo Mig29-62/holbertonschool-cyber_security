b7074e4fd5891fb7ee17a35b5e30920ef97a56b4
mig29-62 <12584@holbertonstudents.com>
2026-05-04 12:51:38 -0400

    holberton

--- web_application_security/0x0c_web_application_foresics/5-users.sh
+++ web_application_security/0x0c_web_application_foresics/5-users.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep "useradd" auth.log | grep "new user" | awk -F'name=' '{print $2}' | awk -F',' '{print $1}' | sort | uniq
+grep "useradd" auth.log | grep "new user" | awk -F'name=' '{print $2}' | awk -F',' '{print $1}' | sort | uniq | paste -sd "," -
