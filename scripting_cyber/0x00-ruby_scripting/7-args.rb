cca6ad862c38bce99698a9edee4290153924bc6f
mig29-62 <12584@holbertonstudents.com>
2026-05-24 15:46:43 -0400

    holberton

---
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+parsed_args = ARGV.flat_map { |arg| arg.split(',') }
+if parsed_args.empty?
+	puts "No arguments provided"
+else 
+   parsed_args.each_with_index do |arg,index|
+        puts "#{index+1}.#{arg}"
+   end
+end 
+
