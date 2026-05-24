c08919bb2f1facaeafcd8a6b0e57f0039dccb5ae
mig29-62 <12584@holbertonstudents.com>
2026-05-24 15:56:07 -0400

    holberton

--- scripting_cyber/0x00-ruby_scripting/7-args.rb
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -1,13 +1,12 @@
 #!/usr/bin/env ruby
 def print_arguments
   parsed_args = ARGV.flat_map { |arg| arg.split(',') }
+
   if parsed_args.empty?
     puts "No arguments provided."
   else
-    puts "Arguments:"
-    parsed_args.each do |arg|
-      puts arg
+    parsed_args.each_with_index do |arg, index|
+      puts "#{index + 1}. #{arg}"
     end
-    puts "" # Adds the trailing newline to match the desired stdout length
   end
 end
