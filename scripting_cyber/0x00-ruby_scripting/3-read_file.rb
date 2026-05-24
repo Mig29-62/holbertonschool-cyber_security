084909cc3769795a3b59d8ae545b04f69facf86a
mig29-62 <12584@holbertonstudents.com>
2026-05-24 14:24:37 -0400

    holberton

--- scripting_cyber/0x00-ruby_scripting/3-read_file.rb
+++ scripting_cyber/0x00-ruby_scripting/3-read_file.rb
@@ -3,10 +3,6 @@ require 'json'
 
 def count_user_ids(path)
   data = JSON.parse(File.read(path))
-  
-  
-  counts = data.tally { |item| item['userId'] }
-
-  
+  counts = data.map { |item| item['userId'] }.tally
   counts.sort.each { |user_id, count| puts "#{user_id}: #{count}" }
 end
